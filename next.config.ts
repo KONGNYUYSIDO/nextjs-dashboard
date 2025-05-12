4b8395126bf13997e3352f7c342cf136d62e74d7
KONGNYUYSIDO <kongnyuyversi24@gmail.com>
2025-05-12 18:26:55 +0000

    second commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // ppr: 'incremental',
+    turbo: {
+      // You can specify turbopack options here
+      // For now we'll leave it empty to just enable turbopack
+    }
+  },
 };
 
 
